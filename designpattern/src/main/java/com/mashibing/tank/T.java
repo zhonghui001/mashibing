79e7e336b1501475c3a6f67c53db1f701142b9f2
zhonghui <qqpy789@163.com>
2020-05-30 22:42:53 +0800

    init

---
+++ designpattern/src/main/java/com/mashibing/tank/T.java
@@ -0,0 +1,13 @@
+package com.mashibing.tank;
+
+import java.awt.Frame;
+
+public class T {
+
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setSize(800,600);
+        f.setResizable(false); //不能改变大小
+        f.setVisible(true);
+    }
+}
