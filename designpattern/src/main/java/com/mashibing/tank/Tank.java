c80ab17e9b1265c868bd00614241ac76fdb45654
zhonghui <qqpy789@163.com>
2020-06-05 22:15:53 +0800

    区分我方和地方坦克

--- designpattern/src/main/java/com/mashibing/tank/Tank.java
+++ designpattern/src/main/java/com/mashibing/tank/Tank.java
@@ -115,6 +115,16 @@ public class Tank {
 
         if (this.group == Group.BAD && random.nextInt(10)>8)
             randomDir();
+
+        //用于防止坦克出界
+        boundsCheck();
+    }
+
+    private void boundsCheck(){
+        if(this.x<0) x=0;
+        if (this.y<30)y=30;
+        if (this.x>TankFrame.GAME_WIDTH-Tank.WIDTH) x=TankFrame.GAME_WIDTH-Tank.WIDTH;
+        if ( this.y>TankFrame.GAME_HEIGHT-Tank.HEIGHT) y = TankFrame.GAME_HEIGHT-Tank.HEIGHT;
     }
 
     private void randomDir(){
